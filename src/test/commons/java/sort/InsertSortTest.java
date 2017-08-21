115991a335c63ac54156f169bd59f2c0f2fb5600
licklulu <1244036947@qq.com>
2017-08-21 18:49:34 +0800

    1

--- src/test/commons/java/sort/InsertSortTest.java
+++ src/test/commons/java/sort/InsertSortTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class InsertSortTest {
 @Test
     public void test() {
-        int[] array={1,3,5,8,2,4,6};
+        int[] array={1,3,9,5,8,2,4,6};
         InsertSort is=new InsertSort();
        is.insertSort(array);
         for (int i = 0; i < array.length; i++) {
